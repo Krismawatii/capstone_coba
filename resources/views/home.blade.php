01d8b7e9a7fcd93ad8ad0302d334fdfa1f674c17
Krismawati <krismawatti11@gmail.com>
2023-06-07 11:41:30 +0700

    home update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,43 +4,111 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Link Bootstrap CSS -->
+
     <title>DarahKu</title>
-    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+
+
 </head>
 
-<body>
-    <nav>
-        <div class="wrapper">
-            <div class="logo"><a href=''>DarahKu</a></div>
-            <div class="menu">
-                <ul>
-                    <li><a href="#home">Home</a></li>
-                    <li><a href="#artikelcekdarah">Artikel</a></li>
-                    <li><a href="#artikelcekdarah">Cek Darah Mu</a></li>
-                    <li><a href="#tutors">Our Team</a></li>
+<body id="home">
+    <script src=<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>></script>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-success shadow fixed-top">
+
+        <div class="container">
+
+            <a class="navbar-brand" href="#">DarahKu</a>
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+
+                <span class="navbar-toggler-icon"></span>
+
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarNav">
+
+                <ul class="navbar-nav ms-auto">
+
                     <li class="nav-item">
-                        <a class="nav-link" href="Login.html">Login</a>
+
+                        <a class="nav-link active" aria-current="page" href="#home">Home</a>
+
+                    </li>
+
+                    <li class="nav-item">
+
+                        <a class="nav-link" href="#about">Artikel</a>
+
+                    </li>
+
+                    <li class="nav-item">
+
+                        <a class="nav-link" href="#design">Cek DarahMu</a>
+
+                    </li>
+
+                    <li class="nav-item">
+
+                        <a class="nav-link" href="#testimoni">Our Team</a>
+
                     </li>
                 </ul>
+
+            </div>
+
+        </div>
+
+    </nav>
+
+    <!-- Jumbotron -->
+
+
+    <section class="jumbotron">
+
+        <div class="container">
+
+            <div class="row justify-content-center text-center">
+
+                <div class="col-mb-3">
+
+                    <img src="image/logo.png" alt="Darahku" width="350" />
+
+                    <h1 class="display-6 fw-bold"></br>Mengapa DarahKu Hadir ?</h1>
+
+                    <p class="lead">Mencegah lebih baik dari pada mengobati. cegah Diabetes, Asam Urat, dan Kolestrol
+                        dengan DarahKu</p>
+                    <p class="desc">
+                    <p>Penyakit Diabetes, Kolestrol, serta Asam Urat ialah penyakit yang rentan bagi tubuh kita. Dengan
+                        Begitu hadirlah DarahKu sebagai platform untuk kita semua guna untuk pengecekan apakah di tubuh kita
+                        terdapat penyakit Diabetes, Kolestrol, serta Asam Urat. Serta kami memberikan solusi untuk mencegah
+                        penyakit tersebut dengan Tips dan Cara pengobatan nya</p>
+                    </p>
+
+                </div>
+
+
+            </div>
+
+        </div>
+
+        </div>
+
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+            <path fill="#BAFFC0" fill-opacity="1" d="M0,256L48,250.7C96,245,192,235,288,224C384,213,480,203,576,208C672,213,768,235,864,213.3C960,192,1056,128,1152,106.7C1248,85,1344,107,1392,117.3L1440,128L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
+        </svg>
+        <div id="copyright">
+            <div class="wrapper">
+                &copy; 2023. <b>DarahKu</b> Dicoding Academy.
             </div>
         </div>
-    </nav>
-    <div class="wrapper">
-        <!-- untuk home -->
-        <section id="home">
-            <img src='img/image_1.png' />
-            <div class="kolom">
-                <p class="deskripsi">Mecegah lebih baik dari pada mengobati. cegah Diabetes, Asam Urat, dan Kolestrol
-                    dengan DarahKu</p>
-                <h2>Mengapa DarahKu Hadir?</h2>
-                <p>Penyakit Diabetes, Kolestrol, serta Asam Urat ialah penyakit yang rentan bagi tubuh kita. Dengan
-                    Begitu hadirlah DarahKu sebagai platform untuk kita semua guna untuk pengecekan apakah di tubuh kita
-                    terdapat penyakit Diabetes, Kolestrol, serta Asam Urat. Serta kami memberikan solusi untuk mencegah
-                    penyakit tersebut dengan Tips dan Cara pengobatan nya</p>
-            </div>
-        </section>
 
-        <!-- untuk courses -->
+    </section>
+
+
+
+    <!-- untuk courses
         <section id="artikelcekdarah">
             <div class="kolom">
                 <h2>Artikel</h2>
@@ -55,10 +123,10 @@
                 <p><a href="" class="tbl-biru">Cek Darah</a></p>
             </div>
             <img src='img/image_2.png' />
-        </section>
+        </section> -->
 
-        <!-- untuk tutors -->
-        <section id="tutors">
+    <!-- untuk tutors -->
+    <!-- <section id="tutors">
             <div class="tengah">
                 <div class="kolom">
                     <h2>Our Team</h2>
@@ -91,10 +159,10 @@
                     </div>
                 </div>
             </div>
-        </section>
+        </section> -->
 
 
-        <div id="contact">
+    <!-- <div id="contact">
             <div class="wrapper">
                 <div class="footer">
                     <div class="footer-section">
@@ -104,11 +172,7 @@
             </div>
         </div>
 
-        <div id="copyright">
-            <div class="wrapper">
-                &copy; 2023. <b>DarahKu</b> Dicoding Academy.
-            </div>
-        </div>
+        <!--  -->
 
 </body>
 
--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -24,7 +24,7 @@
                             <span class="icon-bar"></span>
                             <span class="icon-bar"></span>
                         </button>
-                        <a class="navbar-brand" href="{{route('home')}}">ANISA - Aplikasi Nilai Santri</a>
+                        <a class="navbar-brand" href="{{route('home')}}">DarahKu</a>
                     </div>
                     <!-- Collect the nav links, forms, and other content for toggling -->
                     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
