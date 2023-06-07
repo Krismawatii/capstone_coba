9fe08f5e3d70df6b168ddcce1abb53088e1eab9e
Krismawati <krismawatti11@gmail.com>
2023-06-07 11:43:17 +0700

    home update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -98,6 +98,7 @@
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
             <path fill="#BAFFC0" fill-opacity="1" d="M0,256L48,250.7C96,245,192,235,288,224C384,213,480,203,576,208C672,213,768,235,864,213.3C960,192,1056,128,1152,106.7C1248,85,1344,107,1392,117.3L1440,128L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
         </svg>
+
         <div id="copyright">
             <div class="wrapper">
                 &copy; 2023. <b>DarahKu</b> Dicoding Academy.
@@ -105,75 +106,6 @@
         </div>
 
     </section>
-
-
-
-    <!-- untuk courses
-        <section id="artikelcekdarah">
-            <div class="kolom">
-                <h2>Artikel</h2>
-                <p>Artikel kesehatan yang kami tawarkan akan membantu anda untuk mendapatkan informasi mengenai
-                    kesehatan</p>
-                <p><a href="artikel.html" class="tbl-biru">Baca Artikel</a></p>
-            </div>
-            <div class="kolom">
-                <h2>Cek DarahMu</h2>
-                <p>Cek Darahmu yuk, biar kamu mengetahui bagaimana tingkat kemungkinan anda terserang penyakit terutama
-                    Diabetes, Kolestrol, serta Asam Urat</p>
-                <p><a href="" class="tbl-biru">Cek Darah</a></p>
-            </div>
-            <img src='img/image_2.png' />
-        </section> -->
-
-    <!-- untuk tutors -->
-    <!-- <section id="tutors">
-            <div class="tengah">
-                <div class="kolom">
-                    <h2>Our Team</h2>
-                </div>
-
-                <div class="tutor-list">
-                    <div class="kartu-tutor">
-                        <a href="https://www.linkedin.com/in/firdaa-nur-syahrani-46b988220/">
-                            <img src='img/ailzaa.png' alt="Ailza Zandra">
-                        </a>
-                        <p>Ailza Zandra</p>
-                    </div>
-                    <div class="kartu-tutor">
-                        <a href="https://www.linkedin.com/in/firdaa-nur-syahrani-46b988220/">
-                            <img src='img/firdaa.png' alt="Firda Nur Syahrani">
-                        </a>
-                        <p>Firda Nur Syahrani </p>
-                    </div>
-                    <div class="kartu-tutor">
-                        <a href="https://www.linkedin.com/in/firdaa-nur-syahrani-46b988220/">
-                            <img src='img/krismaa.png' alt="Krismawati">
-                        </a>
-                        <p>Krismawati</p>
-                    </div>
-                    <div class="kartu-tutor">
-                        <a href="https://www.linkedin.com/in/firdaa-nur-syahrani-46b988220/">
-                            <img src='img/rizkyy.png' alt="Rizky Dwi Tiarto">
-                        </a>
-                        <p>Rizky Dwi Tiarto</p>
-                    </div>
-                </div>
-            </div>
-        </section> -->
-
-
-    <!-- <div id="contact">
-            <div class="wrapper">
-                <div class="footer">
-                    <div class="footer-section">
-                        <img src='img/DarahKu.png' />
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <!--  -->
-
 </body>
 
 </html>
